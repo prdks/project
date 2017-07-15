20e412938f83079083fbc2eae15f82b4e857dd4d
littleabone <the.flyingdog@msn.com>
2017-07-16 03:32:30 +0700

    message

--- pages/pdf.php
+++ pages/pdf.php
@@ -40,6 +40,7 @@
 	  }
 
 //-------------เช็คค่าว่าง --------------------//
+$id = $_GET['id'];
 $sql="SELECT  * FROM reservation r
 LEFT JOIN cars c
 ON r.car_id = c.car_id
@@ -53,7 +54,7 @@ LEFT JOIN position po
 ON p.position_id = po.position_id
 LEFT JOIN department d
 ON p.department_id = p.department_id
-WHERE r.reservation_id = 4
+WHERE r.reservation_id = 5
 GROUP BY r.reservation_id";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
@@ -95,6 +96,7 @@ $row = $result->fetch_assoc();
 			<td width="5%"></td>
 			<td width="5%"></td>
 		</tr>
+		<!-- วันที่ขวา -->
 		<tr>
 			<td></td>
 			<td></td>
@@ -117,210 +119,371 @@ $row = $result->fetch_assoc();
 			<td></td>
 			<td></td>
 		</tr>
+		<!-- เรียน ... -->
 		<tr>
 			<td>เรียน</td>
 			<td colspan="19">หฟกฟหกฟหก</td>
 		</tr>
-		<tr height="10px">
-			<td colspan="20">&nbsp;</td>
-		</tr>
-		<tr height="10px"></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- ขื่อ นามสกุลคนทำเรื่อง -->
 		<tr>
 			<td colspan="3"></td>
 			<td colspan="9">ข้าพเจ้า <?php echo $row['title_name'].$row['personnel_name']; ?></td>
 			<td colspan="8">ตำแหน่ง <?php echo $row['position_name']; ?></td>
 		</tr>
+		<!-- ประโยคต้นคำร้อง -->
 		<tr>
 			<td colspan="20">ขออนุญาตใช้รถยนต์<?php echo $row['department_name']; ?> พร้อมพนักงานขับรถยนต์</td>
 		</tr>
+		<!-- สถานที่ -->
 		<tr>
-			<td colspan="4">เพื่อเดินทางไปที่</td>
-			<td colspan="16"><?php echo $row['location']; ?></td>
+			<td colspan="3">เพื่อเดินทางไปที่</td>
+			<td colspan="17"><?php echo $row['location']; ?></td>
 		</tr>
+		<!-- รายละเอียด -->
 		<tr>
 			<td colspan="20" style="text-align: justify;">สำหรับปฏิบัติราชการ <?php echo $row['requirement_detail']; ?></td>
 		</tr>
-		<tr>
-			<td colspan="2">มีคนนั่ง</td>
-			<td colspan="1"><?php echo $row['passenger_total']; ?></td>
-			<td colspan="1">คน</td>
-			<td colspan="1"></td>
-			<?php
-			$sql2 = "
-			SELECT p.* FROM passenger p
-			LEFT JOIN department d
-			ON p.department_id = d.department_id
-			LEFT JOIN reservation r
-			ON p.reservation_id = r.reservation_id
-			WHERE p.reservation_id = '".$row['reservation_id']."'
-			ORDER BY p.passenger_name ASC";
-			$a = $conn->query($sql2);
-			$count = 0;
+		<!-- รายชื่อคนไป -->
+		<?php
+		if ($row['passenger_total'] != 0)
+		{
 			?>
-			<?php while ($b = $a->fetch_assoc()) {
-					$count++;
-						if ($count == 1)
-						{
-							?>
-							<td colspan="15"><?php echo $count.". ".$b['passenger_name']; ?></td>
-						</tr>
-							<?php
-						}
-						else
-						{
-							?>
-							<tr>
-								<td colspan="5"></td>
+			<tr>
+				<td colspan="3">มีคนนั่ง <?php echo $row['passenger_total']; ?> คน</td>
+				<td colspan="2">ดังนี้</td>
+				<?php
+				$sql2 = "
+				SELECT p.* FROM passenger p
+				LEFT JOIN department d
+				ON p.department_id = d.department_id
+				LEFT JOIN reservation r
+				ON p.reservation_id = r.reservation_id
+				WHERE p.reservation_id = '".$row['reservation_id']."'
+				ORDER BY p.passenger_name ASC";
+				$a = $conn->query($sql2);
+				$count = 0;
+				?>
+				<?php while ($b = $a->fetch_assoc()) {
+						$count++;
+							if ($count == 1)
+							{
+								?>
 								<td colspan="15"><?php echo $count.". ".$b['passenger_name']; ?></td>
 							</tr>
-							<?php
-						}
-			}
+								<?php
+							}
+							else
+							{
+								?>
+								<tr>
+									<td colspan="5"></td>
+									<td colspan="15"><?php echo $count.". ".$b['passenger_name']; ?></td>
+								</tr>
+								<?php
+							}
+				}
+			?>
+			<?php
+		}
 		?>
+
 
 		<?php
-		if ($row['date_start'] === $row['date_end'])
+		if ($row['date_start'] === $row['date_end']) //ถ้าไปวันเดียว
 		{
 			?>
 			<tr>
-				<td colspan="6">ในวันที่</td>
-				<td colspan="8"><?php echo DateThai($row['date_start']); ?></td>
-				<td colspan="1">เวลา</td>
-				<td colspan="6"><?php echo TimeThai($row['reserv_stime'])." ถึง ".TimeThai($row['reserv_stime']); ?></td>
+				<td colspan="6">
+					ในวันที่ <?php echo DateThai($row['date_start']); ?>
+				</td>
+				<td colspan="14">
+					ตั้งแต่เวลา <?php echo TimeThai($row['reserv_stime'])." ถึง ".TimeThai($row['reserv_etime']); ?></td>
 			</tr>
 			<?php
 		}
-		else
+		else //หลายวัน
 		{
 			?>
 			<tr>
-				<td colspan="6">ในวันที่</td>
-				<td colspan="8"><?php echo DateThai($row['date_start']); ?></td>
-				<td colspan="1">เวลา</td>
-				<td colspan="4"><?php echo TimeThai($row['reserv_stime']); ?></td>
-			</tr>
-			<tr>
-				<td colspan="6">ถึงวันที่</td>
-				<td colspan="8"><?php echo DateThai($row['date_end']); ?></td>
-				<td colspan="1">เวลา</td>
-				<td colspan="4"><?php echo TimeThai($row['reserv_etime']); ?></td>
+				<td colspan="6">ในวันที่ <?php echo DateThai($row['date_start']); ?></td>
+				<td colspan="3">เวลา <?php echo TimeThai($row['reserv_stime']); ?></td>
+				<td colspan="6">&nbsp;&nbsp;ถึงวันที่ <?php echo DateThai($row['date_end']); ?></td>
+				<td colspan="5">เวลา <?php echo TimeThai($row['reserv_etime']); ?></td>
 			</tr>
 			<?php
 		}
 		?>
-
-		<tr height="20px"></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- ลงชื่อคนทำเรื่อง ขวา -->
 		<tr>
 			<td colspan="12"></td>
 			<td colspan="8" align="center">
 				ลงชื่อ
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;
 				<?php echo $row['personnel_name']; ?>
-				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;
 				ผู้ขออนุมัติ
 			</td>
-
 		</tr>
+		<!-- วงเล็บคนทำเรื่อง ขวา -->
 		<tr>
 			<td colspan="12"></td>
 			<td colspan="8" align="center">
-				(
-				&nbsp;
-				<?php echo $row['title_name'].$row['personnel_name'];?>
-				&nbsp;
-				)
+				(<?php echo $row['title_name'].$row['personnel_name'];?>)
 				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 			</td>
 		</tr>
 		<tr height="30px"></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- เรียน คณบดี ซ้าย -->
 		<tr>
 			<td>เรียน</td>
 			<td colspan="11">คณบดี</td>
-			<td colspan="8" align="center">คำสั่ง</td>
+			<!-- คำสั่ง ขวา -->
+			<td colspan="7" align="center">คำสั่ง&nbsp;</td>
+			<td></td>
 		</tr>
+		<!-- โปร.. ทะเบียน ซ้าย -->
 		<tr>
 			<td></td>
 			<td colspan="11">เพื่อโปรดพิจารณาอนุมัติรถยนต์หมายเลขทะเบียน</td>
+			<!-- checkbox ขวา -->
+			<td colspan="3" align="center">
+				<?php if ($row['reservation_status'] == 1) {
+					?><input type="checkbox" checked="true"><?php
+				}else {
+					?><input type="checkbox"><?php
+				} ?>
+				อนุมัติ
+			</td>
+			<td colspan="4" align="center">
+				<?php if ($row['reservation_status'] == 2 || $row['reservation_status'] == 3) {
+					?><input type="checkbox" checked="true"><?php
+				}else {
+					?><input type="checkbox" ><?php
+				} ?>
+				ไม่อนุมัติ
+			</td>
 			<td></td>
-			<td colspan="2">อนุมัติ</td>
-			<td></td>
-			<td colspan="2">ไม่อนุมัติ</td>
 		</tr>
 		<tr height="20px"></tr>
+		<!-- ทะเบียนรถ -->
 		<tr>
 			<td></td>
-			<td colspan="19"><?php echo $row['car_reg']; ?></td>
+			<td colspan="11"><?php echo $row['car_reg']; ?></td>
+		<!-- ช่องว่างใส่หมายเหตุอนุมัติ -->
+			<td colspan="7">
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<u>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;
+				</u>
+			</td>
+			<td></td>
 		</tr>
-		<tr height="30px"></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- ช่องเซ็น -->
 		<tr>
-			<td colspan="2"></td>
-			<td colspan="5" align="center">ลายเซน</td>
-			<td colspan="5"></td>
-			<td colspan="1">ลงชื่อ</td>
-			<td colspan="7"></td>
-		</tr>
-		<tr>
-			<td colspan="2"></td>
-			<td colspan="5" align="center">(นาง เสาวนี หกฟหก)</td>
-			<td colspan="6"></td>
-			<td colspan="1">(</td>
+			<!-- ซ้าย -->
+			<td colspan="1"></td>
+			<td colspan="1"></td>
+			<td colspan="6">
+				&nbsp;<u>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;
+				</u>
+			</td>
 			<td colspan="3"></td>
-			<td colspan="3">)</td>
+			<!-- ขวา -->
+			<td colspan="1">ลงชื่อ</td>
+			<td colspan="7" >
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<u>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;
+				</u>
+		</td>
+			<td colspan="1"></td>
 		</tr>
+		<!-- ช่องวงเล็บ -->
 		<tr>
+			<!-- ซ้าย -->
 			<td colspan="2"></td>
-			<td colspan="5" align="center">ตำแหน่ง หัวหน้าสำนักคณบดี</td>
-			<td colspan="5"></td>
-			<td colspan="7" align="center">ตำแหน่ง คณบดี/รองคณบดี/ปฏิบัติราชการแทนคณบดี</td>
+			<td colspan="6">
+				&nbsp;(
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				)
+			</td>
+			<td colspan="3"></td>
+			<!-- ขวา -->
+			<td colspan="1"></td>
+			<td colspan="7">
+				&nbsp;&nbsp;&nbsp;
+				(&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				)</td>
+			<td colspan="1"></td>
 		</tr>
+		<!-- บอกตำแหน่งผู้เซ็น -->
 		<tr>
-			<td colspan="12"></td>
-			<td colspan="7" align="center">วันที่ 1 / ก.ย. / 2560</td>
+			<!-- ซ้าย -->
+			<td colspan="2"></td>
+			<td colspan="6" align="center">ตำแหน่ง หัวหน้าสำนักคณบดี</td>
+			<!-- ขวา -->
+			<td colspan="2"></td>
+			<td colspan="10" align="center">ตำแหน่ง คณบดี/รองคณบดีปฏิบัติราชการแทนคณบดี</td>
 		</tr>
-		<tr height="30px"></tr>
+		<!-- วันที่เซ็น ขวา -->
+		<tr>
+			<td colspan="13"></td>
+			<td colspan="7">วันที่ ......................................</td>
+		</tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- สำหรับช่องระหว่างบรรทัด --><tr><td></td></tr>
+		<!-- Head บันทึกเวลา -->
 		<tr>
 			<td colspan="20" align="center"><h4>บันทึกพนักงานขับรถยนต์และยามรักษาการณ์</h4></td>
 		</tr>
+		<?php
+		if ($row['date_start'] === $row['date_end']) //ถ้าไปวันเดียว
+		{
+			?>
+			<!-- ทะเบียนรถยนต์ -->
+			<tr>
+				<td colspan="13">รถยนต์ หมายเลขทะเบียน <?php echo $row['car_reg']; ?></td>
+				<td colspan="7">วันที่ ......................................</td>
+			</tr>
+			<!-- บันทึกเวลาออกจากม. -->
+			<tr>
+				<td colspan="9">เวลาออก ...........................................................</td>
+				<td>น.</td>
+				<td colspan="1"></td>
+				<td colspan="8">เลขกิโลเมตรเมื่อออก .................................</td>
+				<td></td>
+			</tr>
+			<!-- บันทึกเวลากลับม. -->
+			<tr>
+				<td colspan="9">เวลากลับ ...........................................................</td>
+				<td>น.</td>
+				<td colspan="1"></td>
+				<td colspan="8">เลขกิโลเมตรเมื่อกลับ .................................</td>
+				<td></td>
+			</tr>
+			<!-- ระยะทางรวม -->
+			<tr>
+				<td colspan="10">รวมระยะทางกิโลเมตร .............................................</td>
+				<td colspan="10"></td>
+			</tr>
+			<?php
+		}
+		else { //หลายวัน
+			?>
+			<!-- ทะเบียนรถยนต์ -->
+			<tr>
+				<td colspan="20">รถยนต์ หมายเลขทะเบียน <?php echo $row['car_reg']; ?></td>
+			</tr>
+			<!-- บันทึกเวลาออกจากม. -->
+			<tr>
+				<td colspan="5">วันที่ ................................</td>
+				<td colspan="4">เวลาออก ................</td>
+				<td>น.</td>
+				<td colspan="1"></td>
+				<td colspan="8">เลขกิโลเมตรเมื่อออก .................................</td>
+				<td></td>
+			</tr>
+			<!-- บันทึกเวลากลับม. -->
+			<tr>
+				<td colspan="5">วันที่ ................................</td>
+				<td colspan="4">เวลากลับ ................</td>
+				<td>น.</td>
+				<td colspan="1"></td>
+				<td colspan="8">เลขกิโลเมตรเมื่อกลับ .................................</td>
+				<td></td>
+			</tr>
+			<!-- ระยะทางรวม -->
+			<tr>
+				<td colspan="10">รวมระยะทางกิโลเมตร .............................................</td>
+				<td colspan="10"></td>
+			</tr>
+			<?php
+		}
+		?>
+		<!-- คนขับเซ็น -->
 		<tr>
-			<td colspan="5">รถตู้ หมายเลขทะเบียน</td>
-			<td colspan="5" align="center">นช 4609 ปราจีนบุรี</td>
-			<td colspan="2"></td>
-			<td colspan="2">วันที่</td>
-			<td colspan="3" align="center">1 / ก.ย./ 2560</td>
+			<td colspan="9"></td>
+			<td colspan="3" align="right">(ลายมือชื่อ)</td>
+			<td colspan="7">
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<u>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;
+				</u>
+			</td>
+			<td></td>
 		</tr>
+		<!-- วงเล็บ -->
+		<?php
+		$sql_driver = "
+		Select * from personnel p
+		LEFT JOIN cars c
+		ON c.personnel_id = p.personnel_id
+		LEFT JOIN title_name t
+		ON p.title_name_id = t.title_name_id
+		WHERE c.car_id = '".$row['car_id']."'
+		";
+		$d = $conn->query($sql_driver);
+		$e = $d->fetch_assoc();
+		?>
 		<tr>
-			<td colspan="4">เวลาออก</td>
-			<td colspan="6" align="center">16.00 น.</td>
+			<td colspan="11"></td>
 			<td colspan="1"></td>
-			<td colspan="4">เลขกิโลเมตรที่ออก</td>
-			<td colspan="2" align="center">10000</td>
+			<td colspan="7" align="center">(<?php echo $e['title_name'].$e['personnel_name']; ?>)</td>
+			<td></td>
 		</tr>
-		<tr>
-			<td colspan="4">เวลากลับ</td>
-			<td colspan="6" align="center">16.00 น.</td>
-			<td colspan="1"></td>
-			<td colspan="4">เลขกิโลเมตรที่กลับ</td>
-			<td colspan="2" align="center">20000</td>
-		</tr>
-		<tr>
-			<td colspan="6">รวมระยะทางกิโลเมตร</td>
-			<td colspan="2" align="center">10000</td>
-		</tr>
+		<!-- ตำแหน่งขับรถ -->
 		<tr>
 			<td colspan="12"></td>
-			<td colspan="1">ลงชื่อ</td>
-			<td colspan="7"></td>
-		</tr>
-		<tr>
-			<td colspan="13"></td>
-			<td colspan="1">(</td>
-			<td colspan="3"></td>
-			<td colspan="3">)</td>
-		</tr>
-		<tr>
-			<td colspan="14"></td>
-			<td colspan="6">พนักงานขับรถ</td>
+			<td colspan="7" align="center">พนักงานขับรถยนต์</td>
+			<td></td>
 		</tr>
 	</table>
 
