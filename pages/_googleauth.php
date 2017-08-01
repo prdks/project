7b22b208ea10a29b5cedeb7b67a4375e3298eeb9
littleabone <the.flyingdog@msn.com>
2017-08-02 03:27:14 +0700

    edit client & key google auth

--- pages/_googleauth.php
+++ pages/_googleauth.php
@@ -1,9 +1,11 @@
 <script type="text/javascript">
-var clientId = '5851659241-7344218r6lr5vn70jjabeq290q9fb3lo.apps.googleusercontent.com';
-var apiKey = 'AIzaSyAfM0jN7_sgFJJiLeA6xiWKGr9U0RPDAFA';
-var scopes = 'https://www.google.com/m8/feeds';
+var clientId = '311898119234-9ak2l5llohjqsaqvkfhtibhu3dp4f8s7.apps.googleusercontent.com';
+var apiKey = 'AIzaSyDATtdNtA9Rj105rKzJ8kZQAMum9kmv4nU';
+var scopes = 'https://www.googleapis.com/auth/spreadsheets';
 var email;
 var s = 0;
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
 function handleClientLoad() {
   // Loads the client library and the auth2 library together for efficiency.
   // Loading the auth2 library is optional here since `gapi.client.init` function will load
@@ -19,7 +21,7 @@ function initClient() {
       discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
       clientId: clientId,
       hosted_domain: '<?php echo $_SESSION['domain_name'];?>',
-      scope: 'profile'
+      scope: scopes
   }).then(function () {
 
     // Listen for sign-in state changes.
@@ -34,9 +36,7 @@ function updateSigninStatus(isSignedIn) {
   // When signin status changes, this function is called.
   // If the signin status is changed to signedIn, we make an API call.
   if (isSignedIn) {
-
     makeApiCall();
-
   }
 }
 
@@ -52,8 +52,9 @@ function handleSignOutClick(event) {
 
 function makeApiCall() {
   // Make an API call to the People API, and print the user's given name.
-  gapi.client.people.people.get({
-          resourceName: 'people/me'
+      gapi.client.people.people.get({
+          'resourceName': 'people/me',
+          'requestMask.includeField': 'person.names,person.emailAddresses'
         }).then(function(response) {
           // console.log(response);
           var email = response.result.emailAddresses[0].value;
@@ -75,10 +76,42 @@ function makeApiCall() {
         }, function(reason) {
           console.log('Error: ' + reason.result.error.message);
         });
-
-
 }
 
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+function listsheet() {
+  initClient();
+  console.log('ok!!');
+  gapi.client.sheets.spreadsheets.values.get({
+    spreadsheetId: '1fLXNymma9Hk3JjMJLuOQpBvdvLRYcrski36VN5kz9KA',
+    range: 'Form responses 1!A2:E',
+  }).then(function(response) {
+    var range = response.result;
+    if (range.values.length > 0) {
+      appendPre('Name, Major:');
+      for (i = 0; i < range.values.length; i++) {
+        var row = range.values[i];
+        // Print columns A and E, which correspond to indices 0 and 4.
+        appendPre(row[0] + ', ' + row[4]);
+      }
+    } else {
+      appendPre('No data found.');
+    }
+  }, function(response) {
+    appendPre('Error: ' + response.result.error.message);
+  });
+}
 </script>
 <script async defer src="https://apis.google.com/js/api.js"
 onload="this.onload=function(){};handleClientLoad()"
