2b25799f70800293c545d2cb240a8be03a5aa744
littleabone <the.flyingdog@msn.com>
2017-08-07 02:24:59 +0700

    edit Alert Usertype

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -46,9 +46,24 @@ $(document).ready(function() {
       e.preventDefault();
       deleteDepartment();
   });
+    // ---------------- User_type ----------------
+    $("#form_insert_usertype").submit(function(e) {
+        e.preventDefault();
+        insertUserType();
+    });
+
+    $("#form_edit_usertype").submit(function(e) {
+        e.preventDefault();
+        editUserType();
+    });
+
+    $("#form_delete_usertype").submit(function(e) {
+        e.preventDefault();
+        deleteUserType();
+    });
 });
 
-
+// ------------- title_name ---------------
 function insertTitle() {
   var title = $('[name=title_name]').val();
     $.ajax({
@@ -200,6 +215,7 @@ function deleteTitle() {
   });
 }
 
+// ------------- position ---------------
 function insertPosition() {
   var position = $('[name=position_name]').val();
     $.ajax({
@@ -351,6 +367,7 @@ function deletePosition() {
   });
 }
 
+// ------------- department ---------------
 function insertDepartment() {
   var department = $('[name=department_name]').val();
     $.ajax({
@@ -496,6 +513,161 @@ function deleteDepartment() {
               html: true,
             },
             function(){ window.location.assign('department.php'); }
+          );
+      }
+    }
+  });
+}
+
+// ------------- user_type ---------------
+function insertUserType() {
+  var user_type_name = $('[name=user_type_name]').val();
+  var level = $('[name=level]').val();
+
+    $.ajax({
+      type: "POST",
+      url: "user_type/controller.php",
+      data: {user_type_name, level, mode: 'insertUserType'},
+      dataType: 'json',
+      success: function(data){
+        if (data.result == 1) //สำเร็จ
+        {
+          swal({
+                title: "เพิ่มข้อมูลสำเร็จ",
+                text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+                 type: "success",
+                timer: 2000,
+                showConfirmButton: false,
+              },
+              function(){ window.location.assign('user_type.php'); }
+            );
+        }
+        else if (data.result == 0) //ไม่สำเร็จ
+        {
+          swal({
+                title: "ไม่สามารถเพิ่มข้อมูลได้<br>กรุณาทำรายการใหม่",
+                text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+                 type: "error",
+                timer: 2000,
+                html: true,
+                showConfirmButton: false,
+              },
+              function(){ window.location.assign('user_type.php'); }
+            );
+        }
+        else if (data.result === 'error')
+        {
+          swal({
+                title: "ข้อมูลนี้มีอยู่แล้ว<br>กรุณาทำรายการใหม่",
+                text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+                 type: "error",
+                timer: 2000,
+                html: true,
+                showConfirmButton: false,
+              },
+              function(){ window.location.assign('user_type.php'); }
+            );
+        }
+      }
+    });
+}
+
+function editUserType() {
+  var user_type = $('[name=txt_update]').val();
+  var level = $('[name=txt_level]').val();
+  var id = $('[name=update_id]').val();
+  $.ajax({
+    type: "POST",
+    url: "user_type/controller.php",
+    data: {user_type, level, id, mode: 'editUserType'},
+    dataType: 'json',
+    success: function(data){
+      if (data.result == 1) //สำเร็จ
+      {
+        swal({
+              title: "แก้ไขข้อมูลสำเร็จ",
+              text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+               type: "success",
+              timer: 2000,
+              showConfirmButton: false,
+            },
+            function(){ window.location.assign('user_type.php'); }
+          );
+      }
+      else if (data.result == 0) //ไม่สำเร็จ
+      {
+        swal({
+              title: "ไม่สามารถแก้ไขข้อมูลได้<br>กรุณาทำรายการใหม่",
+              text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+               type: "error",
+              timer: 2000,
+              html: true,
+              showConfirmButton: false,
+            },
+            function(){ window.location.assign('user_type.php'); }
+          );
+      }
+      else if (data.result === 'error')
+      {
+        swal({
+              title: "ไม่สามารถแก้ไขข้อมูลได้<br>กรุณาทำรายการใหม",
+              text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+               type: "error",
+              timer: 2000,
+              showConfirmButton: false,
+              html: true,
+            },
+            function(){ window.location.assign('user_type.php'); }
+          );
+      }
+    }
+  });
+}
+
+function deleteUserType() {
+  var id = $('[name=delete_id]').val();
+  $.ajax({
+    type: "POST",
+    url: "user_type/controller.php",
+    data: {id, mode: 'deleteUserType'},
+    dataType: 'json',
+    success: function(data){
+      if (data.result == 1) //สำเร็จ
+      {
+        swal({
+              title: "ลบข้อมูลสำเร็จ",
+              text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+               type: "success",
+              timer: 2000,
+              showConfirmButton: false,
+            },
+            function(){ window.location.assign('user_type.php'); }
+          );
+      }
+      else if (data.result == 0) //ไม่สำเร็จ
+      {
+        swal({
+              title: "ไม่สามารถลบข้อมูลได้<br>กรุณาทำรายการใหม่",
+              text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+               type: "error",
+              timer: 2000,
+              showConfirmButton: false,
+              html: true,
+            },
+            function(){ window.location.assign('user_type.php'); }
+          );
+      }
+      else if (data.result === 'error')
+      {
+        swal({
+              title: "ไม่สามารถลบข้อมูลได้<br>เนื่องจากมีการใช้งานอยู่",
+              text: "แจ้งเตือนจะปิดเองภายใน 2 วินาที",
+               type: "error",
+              timer: 2000,
+              showConfirmButton: false,
+              html: true,
+            },
+            function(){ window.location.assign('user_type.php'); }
           );
       }
     }
--- pages/user_type/modal.php
+++ pages/user_type/modal.php
@@ -8,7 +8,7 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">เพิ่มข้อมูลประเภทผู้ใช้งาน</h4>
       </div>
-      <form id="insert_usertype_form" class="form-horizontal" action="user_type/insert.php" method="post">
+      <form id="form_insert_usertype" class="form-horizontal">
       <div class="modal-body">
         <div class="form-group">
           <label class="col-lg-3 col-md-3 col-sm-3 col-xs-12 control-label">
@@ -50,7 +50,7 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">แก้ไขข้อมูลประเภทผู้ใช้งาน</h4>
       </div>
-      <form id="edit_usertype_form" class="form-horizontal" action="user_type/edit.php" method="post">
+      <form id="form_edit_usertype" class="form-horizontal">
       <div class="modal-body">
         <div class="form-group">
           <label class="col-lg-3 col-md-3 col-sm-3 col-xs-12 control-label">
@@ -58,7 +58,7 @@
           </label>
           <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
             <input type="text" class="form-control" id="show_update"
-            name="str" placeholder="พิมพ์ข้อมูลประเภทผู้ใช้งาน" required>
+            name="txt_update" placeholder="พิมพ์ข้อมูลประเภทผู้ใช้งาน" required>
           </div>
         </div>
         <div class="form-group">
@@ -67,10 +67,10 @@
           </label>
           <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
             <input type="number" class="form-control" id="show_level"
-            name="level" min="-1" max="50" placeholder="0" required>
+            name="txt_level" min="-1" max="50" placeholder="0" required>
           </div>
         </div>
-        <input type="hidden" id="update_id" name="id" value="">
+        <input type="hidden" id="update_id" name="update_id" value="">
       </div>
       <div class="modal-footer">
         <button type="reset" class="btn btn-default" data-dismiss="modal">ยกเลิก</button>
@@ -89,14 +89,14 @@
 
     <!-- Modal content-->
     <div class="modal-content">
-      <form id="delete_usertype_form" class="form-horizontal" action="user_type/delete.php" method="post">
+      <form id="form_delete_usertype" class="form-horizontal">
         <div class="modal-body">
           <div class="form-group text-center">
               <br />
               <p>คุณต้องการลบข้อมูลนี้ใช่หรือไม่</p>
               <b><p id="show_delete"></p></b>
           </div>
-          <input type="hidden" id="delete_id" name="id" value="">
+          <input type="hidden" id="delete_id" name="delete_id" value="">
         </div>
         <div class="modal-footer">
             <button type="reset" class="btn btn-default" data-dismiss="modal">ยกเลิก</button>
